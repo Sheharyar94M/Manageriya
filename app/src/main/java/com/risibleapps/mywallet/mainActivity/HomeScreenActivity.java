46addc752d89aabbbdbdf4c18e03965799a042b6
hammadzaheen <khanishammad7@gmail.com>
2022-06-20 21:53:08 +0500

    app open & exit ads implemented

--- app/src/main/java/com/risibleapps/mywallet/mainActivity/HomeScreenActivity.java
+++ app/src/main/java/com/risibleapps/mywallet/mainActivity/HomeScreenActivity.java
@@ -3,12 +3,15 @@ package com.risibleapps.mywallet.mainActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -23,12 +27,16 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.ads.nativetemplates.NativeTemplateStyle;
+import com.google.android.ads.nativetemplates.TemplateView;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.nativead.NativeAd;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.risibleapps.mywallet.BuildConfig;
@@ -66,6 +74,7 @@ public class HomeScreenActivity extends AppCompatActivity implements NavigationV
 
     private InterstitialAd mInterstitialAd;
 
+    private String nativeAdvanceId="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -145,19 +154,8 @@ public class HomeScreenActivity extends AppCompatActivity implements NavigationV
         }
         else {
 
-            AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
-
-            alertDialog.setTitle("Exit App")
-                    .setMessage("Want to exit app?")
-                    .setPositiveButton("EXIT", (dialogInterface, i) -> {
-                        super.onBackPressed();
-                    })
-                    .setNegativeButton("CANCEL", (dialogInterface, i) -> {
-                        dialogInterface.dismiss();
-                    });
-
-            alertDialog.create().show();
-
+            //calling the exit alert dialog
+            exitAlertDialog();
         }
     }
 
@@ -437,6 +435,52 @@ public class HomeScreenActivity extends AppCompatActivity implements NavigationV
     @Override
     protected void onDestroy() {
         mInterstitialAd = null;
+
         super.onDestroy();
+    }
+
+    private void exitAlertDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+
+        builder.setTitle("Exit App")
+                .setMessage("Want to exit app?");
+        builder.setView(this.getLayoutInflater().inflate(R.layout.exit_dialog,null));
+        AlertDialog alertDialog=builder.create();
+
+        //alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.show();
+
+        TemplateView templateView=alertDialog.findViewById(R.id.ad_template_view);
+
+        AppCompatButton buttonExit=alertDialog.findViewById(R.id.btn_exit);
+        AppCompatButton buttonCancel=alertDialog.findViewById(R.id.btn_cancel);
+
+
+        //click listener buttons
+        buttonExit.setOnClickListener(view -> super.onBackPressed());
+
+        buttonCancel.setOnClickListener(view -> alertDialog.dismiss());
+
+        if(BuildConfig.DEBUG)
+        {
+            nativeAdvanceId=getString(R.string.native_advance_debug);
+        }
+        else {
+            nativeAdvanceId=getString(R.string.native_advance_release);
+        }
+
+        AdLoader adLoader=new AdLoader.Builder(this,nativeAdvanceId)
+                .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                    @Override
+                    public void onNativeAdLoaded(@NonNull NativeAd nativeAd) {
+
+                        NativeTemplateStyle style = new NativeTemplateStyle.Builder().build();
+                        templateView.setStyles(style);
+                        templateView.setNativeAd(nativeAd);
+                    }
+                })
+                .build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
     }
 }
--- app/src/main/java/com/risibleapps/mywallet/splash/SplashScreen.java
+++ app/src/main/java/com/risibleapps/mywallet/splash/SplashScreen.java
@@ -145,4 +145,5 @@ public class SplashScreen extends AppCompatActivity {
         mInterstitialAd = null;
         super.onDestroy();
     }
+
 }
